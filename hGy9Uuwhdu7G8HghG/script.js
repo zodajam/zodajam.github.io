15b4122ad93236a4f41708b89f2f73b172a64174
= <rasmus.larvia@gmail.com>
2025-05-11 19:41:27 +0200

    rewrote code for admin login page and added event listener to check if user presses Enter button

--- hGy9Uuwhdu7G8HghG/script.js
+++ hGy9Uuwhdu7G8HghG/script.js
@@ -1,10 +1,14 @@
-var passwordBox = document.getElementById("passwordBox");
-var uahdibWIUb = "p";var njiuABGjawbniudbNASJguaw = "9";var iTyG8aDF$aAFA = "k";var HAIUWhidgHAdyigd = "h";var UAHdigAIdiuasdiuAGW = "c";var hAIUEghiauwbdjs = "9";var UGOhiuawhfi = "t";var uHAIUYWhdiasdhjabwyd = "u";var ygIAgIAObA = "e";
-function call() {
-    if(passwordBox.value === iTyG8aDF$aAFA + ygIAgIAObA + UGOhiuawhfi + UAHdigAIdiuasdiuAGW + HAIUWhidgHAdyigd + uHAIUYWhdiasdhjabwyd + uahdibWIUb + njiuABGjawbniudbNASJguaw + hAIUEghiauwbdjs) {
-        location.href = "gy7Gygw63Yhbg6323gyiYGRbY3";
-        localStorage.admin = "true";
-    } else {
-        alert("That is the wrong password.");
+const passwordBox = document.getElementById("passwordBox");
+passwordBox.addEventListener("keyup", (event) => {
+    if(event.key == "Enter") {
+        if(passwordBox.value === iTyG8aDF$aAFA + ygIAgIAObA + UGOhiuawhfi + UAHdigAIdiuasdiuAGW + HAIUWhidgHAdyigd + uHAIUYWhdiasdhjabwyd + uahdibWIUb + njiuABGjawbniudbNASJguaw + hAIUEghiauwbdjs) {
+            location.href = "gy7Gygw63Yhbg6323gyiYGRbY3";
+            localStorage.admin = "true";
+        } else {
+            alert("That is the wrong password.");
+            passwordBox.value = "";
+        }
     }
-}
+});
+
+const uahdibWIUb = "p";const njiuABGjawbniudbNASJguaw = "9";const iTyG8aDF$aAFA = "k";const HAIUWhidgHAdyigd = "h";const UAHdigAIdiuasdiuAGW = "c";const hAIUEghiauwbdjs = "9";const UGOhiuawhfi = "t";const uHAIUYWhdiasdhjabwyd = "u";const ygIAgIAObA = "e";
