83f6f9f3d2188ba494c9e9de65380c5d4a4ad820
= <rasmus.larvia@gmail.com>
2025-07-23 18:27:20 +0200

    added iframe to bewbew

--- bewbew/script.js
+++ bewbew/script.js
@@ -2,9 +2,7 @@ const input = document.getElementById("input");
 
 input.addEventListener("keyup", (event) => {
     if(event.key === "Enter") {
-        search();
+        document.getElementById("searchFrame").src = `https://bing.com/search?q=${input.value}`;
         input.value = "";
     }
 });
-
-function search() { window.open(`https://duckduckgo.com/?q=${input.value}`); }
