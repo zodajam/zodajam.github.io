8629cb8e81448db2c1104c81e9fca382c7f37aeb
= <rasmus.larvia@gmail.com>
2025-07-24 15:16:38 +0200

    added localstorage to ball clicker and added 2 different pop sounds when click

--- ball-clicker/script.js
+++ ball-clicker/script.js
@@ -3,7 +3,15 @@ let circle = document.getElementById("circle");
 
 let score = 0;
 
+var audio = new Audio(`Pop${Math.floor(Math.random() * 3)+ 1}.wav`);
+
 circle.addEventListener("click", function() {
     score++;
-    scoreElement.textContent = score;
+    audio = new Audio(`Pop${Math.floor(Math.random() * 3)+ 1}.wav`);
+    audio.play();
+    localStorage.score = score;
+    scoreElement.textContent = localStorage.score;
 });
+
+scoreElement.textContent = localStorage.score;
+score = localStorage.score;
