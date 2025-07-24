34840325e6940796f1d85005e5a78b7ffb3c6530
= <rasmus.larvia@gmail.com>
2025-07-24 23:54:59 +0200

    added soundboard to unlisted projects

--- hax/level2/index.js
+++ hax/level2/index.js
@@ -1,23 +1,4 @@
-const btn = document.getElementById("btn");
-const input = document.getElementById("input");
-
 sessionStorage.clear();
-
-input.addEventListener("keyup", (event) => {
-    if(event.key === "Enter") {
-        if(input.value === sessionStorage.password) {
-            alert("Congrats!!! You have won the medium level");
-            input.value = "";
-        } else {
-            alert("Wrong password!");
-            input.value = "";
-        }
-    }
-});
-
-btn.onclick = function() {
-    sessionStorage.password = Math.floor(Math.random() * 1000) + 1;
-}
 
 const ball = document.getElementById("ball");
 let score = 0;
@@ -25,4 +6,24 @@ let score = 0;
 ball.addEventListener("click", function() {
     score++;
     document.getElementById("clicks").textContent = score;
+});
+
+const btn = document.getElementById("btn");
+
+btn.onclick = function() {
+    sessionStorage.greg = Math.floor(Math.random() * 100) + 1 + Math.floor(Math.random() * 10000) + 1;
+}
+
+const input = document.getElementById("input");
+
+input.addEventListener("keyup", (event) => {
+    if(event.key === "Enter") {
+        if(input.value === sessionStorage.greg) {
+            alert("Congrats!!! You have won the hard level");
+            input.value = "";
+        } else {
+            alert("Wrong password!");
+            input.value = "";
+        }
+    }
 });
--- hax/level3/index.js
+++ hax/level3/index.js
@@ -1,26 +1,38 @@
-sessionStorage.clear();
-
-const ball = document.getElementById("ball");
-let score = 0;
-
-ball.addEventListener("click", function() {
-    score++;
-    if(score === 100) {
-        sessionStorage.key = Math.floor(Math.random() * 100000) + 1;
-    }
-    document.getElementById("clicks").textContent = score;
-});
-
+const btn = document.getElementById("btn");
 const input = document.getElementById("input");
+
+function upload() {
+    const file = document.getElementById("file");
+    if (file) {
+        var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+        var filename = fullPath.substring(startIndex);
+        if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+            filename = filename.substring(1);
+        }
+        alert(filename);
+    }
+}
 
 input.addEventListener("keyup", (event) => {
     if(event.key === "Enter") {
-        if(input.value === sessionStorage.key) {
-            alert("Congrats!!! You have won the hard level");
+        if(input.value === sessionStorage.key.toString()) {
+            alert("Congrats!!! You have won the medium level");
             input.value = "";
         } else {
             alert("Wrong password!");
             input.value = "";
         }
     }
+});
+
+btn.onclick = function() {
+    sessionStorage.password = Math.floor(Math.random() * 1000) + 1;
+}
+
+const ball = document.getElementById("ball");
+let score = 0;
+
+ball.addEventListener("click", function() {
+    score++;
+    document.getElementById("clicks").textContent = score;
 });
