e5dbf3a1a263cd601d276b1f15e8a92e28c332ef
= <rasmus.larvia@gmail.com>
2025-07-25 12:34:32 +0200

    added auto clicker to ball clicker

--- ball-clicker/script.js
+++ ball-clicker/script.js
@@ -10,9 +10,6 @@ if (!isNaN(newScore)) {
 
 circle.addEventListener("click", function() {
     score++;
-    if(localStorage.getItem("score") === "1000000") {
-      alert("YOU HAVE REACHED ONE MILLION CLICKS! YOU WILL NOW GET ACCESS TO ADMIN PAGE! Go to uBash and type \"admin ketchup99\"");
-    }
     let audio = new Audio(`Pop${Math.floor(Math.random() * 2)+ 1}.wav`);
     audio.play();
     localStorage.setItem("score", score);
@@ -20,3 +17,18 @@ circle.addEventListener("click", function() {
 });
 
 scoreElement.textContent = localStorage.getItem("score");
+
+const autoclickerBtn = document.getElementById("autoclicker");
+
+autoclickerBtn.onclick = function() {
+  if(score >= 2500) {
+    score -= 2500;
+    setInterval(function() {
+      score++;
+      let audio = new Audio(`Pop${Math.floor(Math.random() * 2)+ 1}.wav`);
+      audio.play();
+      localStorage.setItem("score", score);
+      scoreElement.textContent = localStorage.getItem("score");
+    }, 1000);
+  }
+}
