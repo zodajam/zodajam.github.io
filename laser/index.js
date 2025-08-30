015f5456ac6f9aa48251ab9e39ada89599daf6bd
= <rasmus.larvia@gmail.com>
2025-08-30 08:04:00 +0200

    made laser not follow cursor but you have to press lmb

--- laser/index.js
+++ laser/index.js
@@ -1,11 +1,30 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-document.addEventListener("mousemove", (event) => {
-    ctx.strokeStyle = "red"; // make it red color
+let drawing;
 
-    ctx.beginPath(); // Start a new path
-    ctx.moveTo(canvas.width / 2, canvas.height / 2); // Move the pen to (30, 50)
-    ctx.lineTo(event.x, event.y); // Draw a line to (150, 100)
+document.addEventListener("mousedown", () => {
+    drawing = true;
+    ctx.strokeStyle = "red";
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, canvas.height / 2);
+    ctx.lineTo(event.x, event.y);
     ctx.stroke();
+    draw();
+});
+
+document.addEventListener("mouseup", () => {
+    drawing = false;
+});
+
+document.addEventListener("mousemove", (event) => {
+    if(drawing) {
+        ctx.strokeStyle = "red"; 
+
+        ctx.beginPath();
+        ctx.moveTo(canvas.width / 2, canvas.height / 2);
+        ctx.lineTo(event.x, event.y); 
+        ctx.stroke();
+    }
 });
