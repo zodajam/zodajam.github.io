dfd14a749c151cd80673d2e2cdaf822d9801fafd
= <rasmus.larvia@gmail.com>
2025-10-16 21:56:15 +0200

    changed bewbew made display larger

--- bewbew/script.js
+++ bewbew/script.js
@@ -2,7 +2,16 @@ const input = document.getElementById("input");
 
 input.addEventListener("keyup", (event) => {
     if(event.key === "Enter") {
+        toggle("search");
         document.getElementById("searchFrame").src = `https://bing.com/search?q=${input.value}`;
         input.value = "";
     }
 });
+
+function toggle(id) {
+    if(document.getElementById(id).style.display === "block") {
+        document.getElementById(id).style.display = "none";
+    } else {
+        document.getElementById(id).style.display = "block";
+    }
+}
