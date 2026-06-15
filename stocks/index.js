154a266cee38631976bb278786926f7a5bbf7009
zodajam <rasmuslarvia@proton.me>
2026-06-15 12:21:44 +0200

    updated stocks

--- stocks/index.js
+++ stocks/index.js
@@ -1,9 +1,14 @@
 const priceText = document.getElementById("price");
 const amountStocks = document.getElementById("stocks");
 const amountNeocoins = document.getElementById("neocoins");
+const updatedPriceText = document.getElementById("updatedPrice");
+const countdownText = document.getElementById("countdown");
 
 let price = 1;
-let stocks = 0;
+if(+localStorage.getItem("stockPrice") > 1) {  // kom på att man kan göra sådär så den kollar om du har en stockPrice som localStorage. kopiera ba den här nästa gång.
+    price = +localStorage.getItem("stockPrice");
+}
+let stocks = +localStorage.getItem("stocks");
 let neocoins = +localStorage.getItem("neocoin");
 
 priceText.innerText = price;
@@ -11,6 +16,7 @@ amountStocks.innerText = stocks;
 amountNeocoins.innerText = neocoins;
 
 function buy() {
+    localStorage.setItem("stocks", stocks);
     if(price <= 0) {
 
     } else {
@@ -23,6 +29,7 @@ function buy() {
 }
 
 function sell() {
+    localStorage.setItem("stocks", stocks);
     if(stocks <= 0) {
         alert("you dont have any stocks dumbass");
     } else {
@@ -39,13 +46,31 @@ let minus;
 
 setInterval(function() {
     let plusOrMinus = Math.floor(Math.random() * 2);
+    let oldPrice = price;
     if(plusOrMinus == 0) { price += Math.floor(Math.random() * 5); }
     if(plusOrMinus == 1) {
         if(price <= 1) {
             price = 1;
+            localStorage.setItem("stockPrice", price);
         } else {
             price -= Math.floor(Math.random() * 5);
         }
     } 
+    localStorage.setItem("stockPrice", price);
+    if(+localStorage.getItem("stockPrice") > oldPrice) { updatedPriceText.textContent = "↑"; updatedPriceText.style.color = "lime"};
+    if(+localStorage.getItem("stockPrice") == oldPrice) { updatedPriceText.textContent = "-"; updatedPriceText.style.color = "orange" };
+    if(+localStorage.getItem("stockPrice") < oldPrice)  { updatedPriceText.textContent = "↓"; updatedPriceText.style.color = "red" };
     priceText.innerText = price;
 }, 10000);
+
+let i = 0;
+
+setInterval(function () {
+    if(i == 10) {
+        countdownText.textContent = 10;
+        i = 0;
+    }
+
+    i++;    
+    countdownText.textContent = 10 - i;
+}, 1000);
