e1e318b35906770e58a46a8dd237de48a72bccfe
rasmuus10 <rasmuslarvia@proton.me>
2026-08-02 20:59:12 +0200

    ncjs update

--- ncjs/index.js
+++ ncjs/index.js
@@ -1,3 +1,22 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let fillStyle = "red";
+ctx.fillStyle = fillStyle;
+
+function rect(x, y, width, height) {
+    ctx.fillRect(x, y, width, height);
+}
+
 function run() {
     eval(document.getElementById("text").value);
+    ctx.fillStyle = fillStyle;
 }
+
+let neocoin = +localStorage.getItem("neocoin");
+
+function plusNeocoin() { +localStorage.setItem("neocoin", neocoin++); }
+
+function setBackground(color) { document.body.style.backgroundColor = color; }
+
+function setColor(color) { document.body.style.color = color; }
